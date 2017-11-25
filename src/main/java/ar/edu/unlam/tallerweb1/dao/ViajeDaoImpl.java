e0f8e148e9fb0f6ad50a1b5634061373d74da2c7
MartoGonzalvo <gonzalvomartin@gmail.com>
2017-11-25 14:16:30 -0300

    cambios nuevos

--- src/main/java/ar/edu/unlam/tallerweb1/dao/ViajeDaoImpl.java
+++ src/main/java/ar/edu/unlam/tallerweb1/dao/ViajeDaoImpl.java
@@ -37,14 +37,30 @@ public class ViajeDaoImpl implements ViajeDao{
 	@Override
 	public List<Viaje> buscarPorPrecioYDestino(float precio, String lugar) {
 		final Session session = sessionFactory.getCurrentSession();
-		Criteria Busqueda =session.createCriteria(Viaje.class);
+		Criteria Busqueda =session.createCriteria(Viaje.class); // creo criteria Busqueda 
 		
-		Busqueda.add(Restrictions.le("precio",precio))
-		.createAlias("transporte", "trans")
-		.add(Restrictions.eq("trans.lugar",lugar));
+		if(precio==0){// filtra por el campo prcio del modelo campo 
+			if(lugar.equals("")){// filtra por el campo lugar del modelo transporte
+			 // si no se le pasa nada en precio, ni lugar lista todo	
+			}else{ // si no se le pasa precio y si lugar(tipo de destino) lista todo lo que coincida con ese destino
+				Busqueda.createAlias("transporte", "trans")
+				.add(Restrictions.eq("trans.lugar",lugar));
+			} 
+		}else{
+			if(lugar.equals("")){// si se le pasa un precio y no un tipo de destino lista todos los que sean < o = a ese importe, sin importar destino
+				Busqueda.add(Restrictions.le("precio",precio));
+			}else{// si se le pasa precio y tipo de destino filtra por ambos campos
+				Busqueda.add(Restrictions.le("precio",precio))
+				.createAlias("transporte", "trans")
+				.add(Restrictions.eq("trans.lugar",lugar));
+			}
+			
+		}
+		
 
-		return  Busqueda.list();
-	//	return session.createCriteria(Viaje.class).add(Restrictions.le("precio", precio)).list();
+
+		return  Busqueda.list(); // devuelve el criteria con los filtros hechos en la validacion
+
 		
 	}
 
